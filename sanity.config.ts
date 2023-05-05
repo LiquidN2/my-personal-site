c82c3cabc67bc44276f260b9f438aa76212e2a0e
Hugh Nguyen <hhnguyen255@gmail.com>
2023-05-05 12:45:15 +0700

    added sanity studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,13 +5,14 @@ const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET as string;
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION as string;
 const title = process.env.NEXT_PUBLIC_SANITY_TITLE as string;
+const basePath = process.env.NEXT_PUBLIC_SANITY_BASE_PATH as string;
 
 const config = defineConfig({
   projectId,
   dataset,
   title,
   apiVersion,
-  basePath: '/admin',
+  basePath,
   plugins: [deskTool()],
 });
 
